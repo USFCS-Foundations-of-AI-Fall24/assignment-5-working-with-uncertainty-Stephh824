9a14acaa8028161f14687813457f8a431e204364
spena <spena7@dons.usfca.edu>
2024-11-10 13:57:46 -0800

    added load() and test case

--- HMM.py
+++ HMM.py
@@ -4,6 +4,10 @@ import random
 import argparse
 import codecs
 import os
+import re
+from collections import defaultdict
+from pathlib import Path
+
 import numpy
 
 # Sequence - represents a sequence of hidden states and corresponding
@@ -27,21 +31,31 @@ class HMM:
         e.g. {'happy': {'silent': '0.2', 'meow': '0.3', 'purr': '0.5'},
               'grumpy': {'silent': '0.5', 'meow': '0.4', 'purr': '0.1'},
               'hungry': {'silent': '0.2', 'meow': '0.6', 'purr': '0.2'}}"""
-
-
-
         self.transitions = transitions
         self.emissions = emissions
 
-    ## part 1 - you do this.
+    # Loading the contents of the basename to add to the proper attribute
     def load(self, basename):
-        """reads HMM structure from transition (basename.trans),
-        and emission (basename.emit) files,
-        as well as the probabilities."""
-        pass
+        types = (".trans", ".emit")
+        for ftype in types :
+            tdict = defaultdict(dict)
+            path = Path(basename + ftype)
+            # Validating that the path exists
+            if not path.is_file() :
+                print("Please enter a valid basename for your .emit and .trans files")
+                return
+            # Opening the file and iterating through the lines
+            with path.open() as f :
+                for line in f :
+                    line = line.split(" ")
+                    tdict[line[0]][line[1]] = line[2].rstrip('\n')
+                # Adding to the proper attribute
+                match ftype:
+                    case ".trans" :
+                        self.transitions = tdict
+                    case ".emit" :
+                        self.emissions = tdict
 
-
-   ## you do this.
     def generate(self, n):
         """return an n-length Sequence by randomly sampling from this HMM."""
         pass
@@ -51,17 +65,18 @@ class HMM:
     ## you do this: Implement the Viterbi algorithm. Given a Sequence with a list of emissions,
     ## determine the most likely sequence of states.
 
-
-
-
-
-
     def viterbi(self, sequence):
         pass
     ## You do this. Given a sequence with a list of emissions, fill in the most likely
     ## hidden states using the Viterbi algorithm.
 
 
+
+if __name__ == "__main__" :
+    # when moving to submission.py it'll look like HMM.HMM()
+    h = HMM()
+    h.load('cat')
+    print(h.transitions)
 
 
 
