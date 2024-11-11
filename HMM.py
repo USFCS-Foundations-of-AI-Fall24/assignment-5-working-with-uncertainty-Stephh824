63fbfe55967633c786adf5ce22348c8240af35ee
spena <spena7@dons.usfca.edu>
2024-11-11 08:28:13 -0800

    added generating a random sequence of n observations using the command line

--- HMM.py
+++ HMM.py
@@ -5,8 +5,10 @@ import argparse
 import codecs
 import os
 import re
+import sys
 from collections import defaultdict
 from pathlib import Path
+from decimal import Decimal
 
 import numpy
 
@@ -43,7 +45,7 @@ class HMM:
             # Validating that the path exists
             if not path.is_file() :
                 print("Please enter a valid basename for your .emit and .trans files")
-                return
+                sys.exit()
             # Opening the file and iterating through the lines
             with path.open() as f :
                 for line in f :
@@ -58,7 +60,13 @@ class HMM:
 
     def generate(self, n):
         """return an n-length Sequence by randomly sampling from this HMM."""
-        pass
+        obs = list()
+        curr_state = random.choices(list(self.transitions["#"].keys()), [float(item) for item in self.transitions["#"].values()])[0]
+        for i in range(0, n) :
+            curr_emit = random.choices(list(self.emissions[curr_state].keys()), [float(item) for item in self.emissions[curr_state].values()])[0]
+            obs.append((curr_state, curr_emit))
+            curr_state = random.choices(list(self.transitions[curr_state].keys()), [float(item) for item in self.transitions[curr_state].values()])[0]
+        return obs
 
     def forward(self, sequence):
         pass
@@ -74,10 +82,16 @@ class HMM:
 
 if __name__ == "__main__" :
     # when moving to submission.py it'll look like HMM.HMM()
-    h = HMM()
-    h.load('cat')
-    print(h.transitions)
+    parser = argparse.ArgumentParser(
+                    prog = 'HMM.py',
+                    description = 'Performs Hidden Markov Models on a sequence of states')
+    parser.add_argument('basename', help = "The basename of the .emit and .trans file to process")
+    parser.add_argument('--generate', action = "store_true", help = "Generate a random sequence with N random observations")
+    args = parser.parse_args()
 
+    h = HMM()
+    h.load(args.basename)
+    print(h.generate(20))
 
 
 
