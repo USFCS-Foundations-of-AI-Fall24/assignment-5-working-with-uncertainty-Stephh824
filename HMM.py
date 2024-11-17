c196fa1fa8c30551468e4ddd028ad78ff2707057
spena <spena7@dons.usfca.edu>
2024-11-17 14:30:49 -0800

    fix matrix row entries, add if lander is safe to land

--- HMM.py
+++ HMM.py
@@ -47,8 +47,9 @@ class HMM:
             # Opening the file and iterating through the lines
             with path.open() as f :
                 for line in f :
-                    line = line.split(" ")
-                    tdict[line[0]][line[1]] = line[2].rstrip('\n')
+                    if len(line) != 1 :
+                        line = line.split(" ")
+                        tdict[line[0]][line[1]] = line[2].rstrip('\n')
                 # Adding to the proper attribute
                 match ftype:
                     case ".trans" :
@@ -58,68 +59,73 @@ class HMM:
 
     def generate(self, n):
         """return an n-length Sequence by randomly sampling from this HMM."""
-        obs = list()
+        #states = list()
+        emissions = list()
         curr_state = random.choices(list(self.transitions["#"].keys()), [float(item) for item in self.transitions["#"].values()])[0]
+        #states.append(curr_state)
         for i in range(0, n) :
             curr_emit = random.choices(list(self.emissions[curr_state].keys()), [float(item) for item in self.emissions[curr_state].values()])[0]
-            obs.append(curr_emit)
+            emissions.append(curr_emit)
             curr_state = random.choices(list(self.transitions[curr_state].keys()), [float(item) for item in self.transitions[curr_state].values()])[0]
-        return obs
+            # states.append(curr_state)
+        print(emissions)
+        return emissions
+
 
     def forward(self, sequence):
-        # Initializing a matrix with sequence + 1 columns to account for sequence and #
-        # and with transitions["#"].keys() + 1 rows to account for starting states and "#"
-        matrix = [[float(0) for j in range(len(sequence) + 1)] for i in range(len(self.transitions["#"].keys()) + 1)]
+        # Initializing a matrix with sequence + 1 columns to account for sequence and "-"
+        # and with transitions["#"].keys() rows to account for all states, including "#"
+        matrix = [[float(0) for j in range(len(sequence) + 1)] for i in range(len(self.transitions.keys()))]
         matrix[0][0] = 1.0
         # Iterating through columns -> sequence
         for i in range(1, len(sequence) + 1):
+            # For initial step since it's not reliant on previous state, only probability of start state
             if i == 1 :
-                # For initial step since it's not reliant on previous state, only probability of start state
-                for idx, state in enumerate(self.transitions["#"].keys()):
-                    # Probability = P(observation | state) -> the emission probability *
-                    #               P(state | "#") -> the start state probability *
-                    #               P("#") -> always 1.0
-                    try:
-                        prob = float(self.emissions[state][sequence[i - 1]]) * float(self.transitions["#"][state])
-                        # if KeyError, means transition or emission is not reachable therefore is 0
-                    except KeyError:
-                        # Means no transitions or emission exists
-                        prob = 0
-                    matrix[idx + 1][i] = prob
+                 for idx, state in enumerate(self.transitions.keys()):
+                     # Probability = P(observation | state) -> the emission probability *
+                     #               P(state | "#") -> the start state probability *
+                     #               P("#") -> always 1.0
+                     try:
+                         prob = float(self.emissions[state][sequence[i - 1]]) * float(self.transitions["#"][state])
+                         # if KeyError, means transition or emission is not reachable therefore is 0
+                     except KeyError:
+                         # Means no transitions or emission exists
+                         prob = 0
+                     matrix[idx][i] = prob
             else :
-                # Iterating through rows (states) -> "#", "happy", "grumpy", "hungry"
-                for idx1, state in enumerate(self.transitions["#"].keys()) :
+                 # Iterating through rows (states) -> "#", "happy", "grumpy", "hungry"
+                 for idx1, state in enumerate(self.transitions.keys()) :
                     prob = 0
-                    # Iterating through states again for prev_state accessibility
-                    for idx2, state2 in enumerate(self.transitions["#"].keys()) :
+                     # Iterating through states again for prev_state accessibility
+                    for idx2, state2 in enumerate(self.transitions.keys()) :
                         # Probability = P(observation | state) -> the emission probability *
                         #               P(state | prev_state) -> the transition probability *
                         #               P(prev_state) -> the previous row entry in the matrix
                         try:
-                            prob += matrix[idx2 + 1][i-1] * float(self.transitions[state2][state]) * float(self.emissions[state][sequence[i-1]])
+                            prob += matrix[idx2][i-1] * float(self.transitions[state2][state]) * float(self.emissions[state][sequence[i - 1]])
                         # if KeyError, means transition or emission is not reachable therefore is 0
                         except KeyError:
                             prob += 0
-                    matrix[idx1 + 1][i] = prob
+                    matrix[idx1][i] = prob
         max_idx = 0
         max_val = 0
         for idx, row in enumerate(matrix):
             if row[len(row) - 1] > max_val:
                 max_val = row[len(row) - 1]
                 max_idx = idx
-        return list(self.transitions["#"].keys())[max_idx - 1]
+        return list(self.transitions.keys())[max_idx]
 
     def viterbi(self, sequence):
         # Initializing a matrix with sequence + 1 columns to account for sequence and #
         # and with transitions["#"].keys() + 1 rows to account for starting states and "#"
-        matrix = [[float(0) for j in range(len(sequence) + 1)] for i in range(len(self.transitions["#"].keys()) + 1)]
-        backpointers = [[float(0) for j in range(len(sequence) + 1)] for i in range(len(self.transitions["#"].keys()) + 1)]
+        matrix = [[float(0) for j in range(len(sequence) + 1)] for i in range(len(self.transitions.keys()))]
+        backpointers = [[float(0) for j in range(len(sequence) + 1)] for i in range(len(self.transitions.keys()))]
         matrix[0][0] = 1.0
         # Iterating through columns -> sequence
         for i in range(1, len(sequence) + 1):
             if i == 1:
                 # For initial step since it's not reliant on previous state, only probability of start state
-                for idx, state in enumerate(self.transitions["#"].keys()):
+                for idx, state in enumerate(self.transitions.keys()):
                     # Probability = P(observation | state) -> the emission probability *
                     #               P(state | "#") -> the start state probability *
                     #               P("#") -> always 1.0
@@ -128,22 +134,22 @@ class HMM:
                     # if KeyError, means transition or emission is not reachable therefore is 0
                     except KeyError:
                         prob = 0
-                    matrix[idx + 1][i] = prob
+                    matrix[idx][i] = prob
             else:
             # Iterating through rows (states) -> "#", "happy", "grumpy", "hungry"
-                for idx1, state in enumerate(self.transitions["#"].keys()):
+                for idx1, state in enumerate(self.transitions.keys()):
                     max_prob = 0
                     # Iterating through states again for prev_state accessibility
-                    for idx2, state2 in enumerate(self.transitions["#"].keys()):
+                    for idx2, state2 in enumerate(self.transitions.keys()):
                         try :
-                            curr_prob = matrix[idx2 + 1][i - 1] * float(self.transitions[state2][state]) * float(self.emissions[state][sequence[i - 1]])
+                            curr_prob = matrix[idx2][i - 1] * float(self.transitions[state2][state]) * float(self.emissions[state][sequence[i - 1]])
                         # if KeyError, means transition or emission is not reachable therefore is 0
                         except KeyError:
                             curr_prob = 0
                         if curr_prob > max_prob:
                             max_prob = curr_prob
-                            backpointers[idx1+1][i] = idx2 + 1
-                    matrix[idx1 + 1][i] = max_prob
+                            backpointers[idx1][i] = idx2
+                    matrix[idx1][i] = max_prob
         states = []
         min_value = float("inf")
         min_row = -1
@@ -165,10 +171,13 @@ class HMM:
         emits = []
         # Ignoring the 0 as it tells us nothing, the entire first (second(?)) row is all zeros
         for state in states[1:] :
-            emits.append(list(self.transitions["#"].keys())[state - 1])
+            emits.append(list(self.transitions.keys())[state])
         return emits
 
-def validate(basename, file, type) :
+def safe_land(state) :
+    return state in ["4,3", "4,4", "3,4", "2,5"]
+
+def run(basename, file, type) :
     h = HMM()
     h.load(basename)
     if not Path(file).is_file() :
@@ -184,7 +193,13 @@ def validate(basename, file, type) :
                     case "forward" :
                         print(f"The most likely current state is %s" % h.forward(tokens))
                     case "viterbi" :
-                        print(f"The most likely sequence of hidden states for the sequence of observations \"%s\" is \"%s\"" % (" ".join(tokens), " ".join(h.viterbi(tokens))))
+                        likely_states = h.viterbi(tokens)
+                        print(f"The most likely sequence of hidden states for the sequence of observations \"%s\" is \"%s\"" % (" ".join(tokens), " ".join(likely_states)))
+                        if basename == "lander" :
+                            if safe_land(likely_states[-1]) :
+                                print("It it safe for the lander to land")
+                            else :
+                                print("It is not safe for the lander to land")
 
 
 if __name__ == "__main__" :
@@ -198,38 +213,14 @@ if __name__ == "__main__" :
     parser.add_argument('--viterbi', metavar = "outfile", help = "Runs the Viterbi Algorithm on the observations in this file, if given a file that doesn't exist, will create one with default 20 observations")
     args = parser.parse_args()
 
-
     if args.forward :
-        validate(args.basename, args.forward, "forward")
+        run(args.basename, args.forward, "forward")
     if args.viterbi :
-        validate(args.basename, args.viterbi, "viterbi")
-    # if args.generate :
-    #     with open(args.basename + "_sequence.obs", 'w') as f :
-    #         h.load(args.basename)
-    #         toks = " ".join(h.generate(int(args.generate)))
-    #         f.write(toks)
-    # if args.forward:
-    #     if Path(args.forward).is_file() :
-    #         h.load(args.basename)
-    #         with open(args.forward) as f :
-    #             for line in f :
-    #                 if len(line) != 1:
-    #                     lines = line.split(" ")
-    #                     tokens = [item.rstrip('\n') for item in lines if item != ('' or '\n')]
-    #                     print(f"The most likely current state is %s" % h.forward(tokens))
-    #     else :
-    #         with open(args.forward, 'w') as f :
-    #             h.load(args.basename)
-    #             toks = " ".join(h.generate(30))
-    #             f.write(toks)
-    #         h.forward(args.forward)
-    # if args.viterbi :
-    #     if Path(args.viterbi).is_file() :
-    #         h.load(args.basename)
-    #         with open(args.viterbi) as f :
-    #             for line in f :
-    #                 if len(line) != 1:
-    #                     lines = line.split(" ")
-    #                     tokens = [item.rstrip('\n') for item in lines if item != ('' or '\n')]
-    #                     print(tokens)
-    #                     h.viterbi(tokens)
+        run(args.basename, args.viterbi, "viterbi")
+    if args.generate :
+        h = HMM()
+        with open(args.basename + "_sequence.obs", 'w') as f :
+            h.load(args.basename)
+            tokens = " ".join(h.generate(int(args.generate)))
+            f.write(tokens)
+
